43ac5ebefa80ee1944ff4808810582153c9e2f3f
Alex Day <alex@alexday.me>
2023-07-17 16:22:23 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# A Study in Zucker: Insights on Human-Robot Interaction
+This repository holds the processed human-robot interaction data that was recorded at Clemson's Zucker Graduate Education Center in May of 2020.
+
+This dataset contains two different agent types (robot and human) in several different scenarios. The robot switched between 3 different motion controllers (Linear, NHTTC, and CADRL) over multiple different scenarios with different permutations of human agents. There are also scenes without the robot for a baseline.
+
+## Using the Dataset
+To facilitate further analysis of our dataset we provide a simple python class to query our dataset based on both the controller and scene that was used. This code is in the `scripts/map_reader.py` file. The class is instantiated with a `map.csv` file and a root directory where the data is stored. An example of querying all files that use the CADRL controller on the Intersection scene is shown below:
+
+```python3
+zdm = ZuckerDatasetMap("./data/map.csv", "./data")
+print(
+    zdm.get_by(scene=Scene.INTERSECTION, controller=Controller.CADRL)
+)
+```
+
+This returns a `frozenset` containing the paths of the csv files. The ID of the agent is the first column and is consistent across the dataset, the robot always has ID -1.
---
+++ scripts/map_reader.py
@@ -0,0 +1,51 @@
+from typing import List, Optional, FrozenSet
+import pandas as pd
+from pathlib import Path
+from enum import Enum
+
+class Controller(Enum):
+    NHTTC = "NHTTC"
+    CADRL = "CADRL"
+    LINEAR = "Non-Reactive"
+    NO_ROBOT = "No Robot"
+
+class Scene(Enum):
+    TWO_VS_ONE = "2v1"
+    OVERTAKE = "Overtake"
+    HEAD_TO_HEAD = "Head-to-Head"
+    OPPOSITE = "3v1 Opposite"
+    ADJACENT = "3v1 Adjacent"
+    PERPENDICULAR = "3v1 90"
+    INTERSECTION = "Intersection"
+    THREE_VS_ONE = "3v1"
+
+class ZuckerDatasetMap:
+    def __init__(self, map_fn: str, root_folder: str):
+        self._map = pd.read_csv(map_fn)
+        self._files = [x for x in Path(root_folder).glob("*csv") if "map" not in x.stem]
+
+    def get_by_controller(self, controller: Controller) -> FrozenSet[Path]:
+        trial_numbers = self._map[self._map.Controller == controller.value]["Trial Number"].values
+        return frozenset([x for x in self._files if int(x.stem.split("_")[-1]) in trial_numbers])
+
+    def get_by_scene(self, scene: Scene) -> FrozenSet[Path]:
+        trial_numbers = self._map[self._map.Scene == scene.value]["Trial Number"].values
+        return frozenset([x for x in self._files if int(x.stem.split("_")[-1]) in trial_numbers])
+
+    def get_by(self, controller: Optional[Controller]=None, scene: Optional[Scene]=None) -> FrozenSet[Path]:
+        matches = []
+        if controller:
+            matches.append(self.get_by_controller(controller))
+        if scene:
+            matches.append(self.get_by_scene(scene))
+
+        if len(matches) == 1:
+            return matches[0] 
+
+        return matches[0] & matches[1]
+
+if __name__ == "__main__":
+    zdm = ZuckerDatasetMap("./data/map.csv", "./data")
+    print(
+        zdm.get_by(scene=Scene.ADJACENT, controller=Controller.CADRL)
+    )
